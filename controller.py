6dad89e3784913e99dd17e86d594bec52c2fb970
gabrielibagon <gabriel.ibagon@gmail.com>
2016-02-01 18:46:21 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,14 +3,15 @@ import open_bci_v3 as bci
 
 board = bci.OpenBCIBoard()
 
-	if not board.streaming:
-		print("Error")
-	else:
-		def __call__(self,sample):
-			sample = board.sample
-			if sample:
-				sample_string = "ID: %f\n%s\n%s" %(sample.id, str(sample.channel_data)[1:-1])
-				print "---------------------------------"
-				print sample_string
-				print "---------------------------------"
+def __call__(self,sample):
+	sample = board.sample
+	if sample:
+		sample_string = "ID: %f\n%s\n%s" %(sample.id, str(sample.channel_data)[1:-1])
+		print "---------------------------------"
+		print sample_string
+		print "---------------------------------"
 
+
+	# if not board.streaming:
+	# 	print("Error")
+	# else:
